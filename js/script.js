e4ade0b4b56754caabc53bbd852427787aae1fb0
ChiaraMameli <mameli.chiara@libero.it>
2022-07-11 14:45:49 +0200

    completed

--- js/script.js
+++ js/script.js
@@ -6,3 +6,14 @@ Aggiungere alla pagina un’immagine, presa anch’essa da un data.
 */
 
 console.log('Vue ok', Vue)
+
+const root = new Vue({
+    el: '#root',
+    data: {
+        firstName: 'Chiara',
+        lastName: 'Mameli',
+        age: '26',
+        birthPlace: 'Cagliari',
+        birthPlaceImage: 'https://www.sardegna.info/wp-content/uploads/sites/5/cagliari-hd.jpg',
+    }
+})
